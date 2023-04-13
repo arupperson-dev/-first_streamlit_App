f167e98cab19f24e2fadf57f2efb6dc4d859d433
arupperson-dev <130658571+arupperson-dev@users.noreply.github.com>
2023-04-13 16:35:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ streamlit.title('Hi, Snowflake Trainning')
 
 streamlit.header('Learning from Documentation')
 streamlit.text('Snowflake University')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
